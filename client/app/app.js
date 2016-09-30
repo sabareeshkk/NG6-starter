93eb14528177b77da6a7365bc3c30f4bd847e5bc
sabareeshk <sabareeshk1991@gmail.com>
2016-09-30 14:22:51 +0530

    new bootstrap template added to the boilerplate

--- client/app/app.js
+++ client/app/app.js
@@ -1,20 +1,30 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import Common from './common/common';
-import Components from './components/components';
+//import Common from './common/common';
+//import Components from './components/components';
 import AppComponent from './app.component';
+
+//importing styles
 import 'normalize.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 angular.module('app', [
-    uiRouter,
-    Common,
-    Components
-  ])
-  .config(($locationProvider) => {
+  uiRouter
+])
+  .config(($locationProvider, $stateProvider, $urlRouterProvider) => {
     "ngInject";
     // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
     // #how-to-configure-your-server-to-work-with-html5mode
     $locationProvider.html5Mode(true).hashPrefix('!');
+
+    $stateProvider
+      .state('app', {
+        url: '/app',
+        template: '<app></app>'
+      });
+
+    // Default page for the router
+    $urlRouterProvider.otherwise('/app');
   })
 
   .component('app', AppComponent);
